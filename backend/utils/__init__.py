26888bf8f63360db07511bca8f00ee6f2a5f9e4d
Tal Alter <talalter95900@gmail.com>
2026-05-22 17:25:40 +0300

    chore: add utils package init

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -2,11 +2,10 @@
 
 __all__ = [
     "split_sentences",
-    "get_stopwords",
     "tokenize",
     "jaccard",
     "deduplicate",
 ]
 
 from backend.utils.dedup import deduplicate, jaccard, tokenize
-from backend.utils.text import get_stopwords, split_sentences
+from backend.utils.text import split_sentences
